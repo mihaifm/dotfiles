1d3959284816b0db6e0c06c26773b49de1fffaee
Mihai C <mihaixc@gmail.com>
2021-08-28 22:57:55 +0300

    Added airline and 4colors

--- install.sh
+++ install.sh
@@ -10,8 +10,14 @@ rm -rf ~/.vim/pack/mybundle/start/bufstop/.git
 git clone https://github.com/mihaifm/vimpanel ~/.vim/pack/mybundle/start/vimpanel
 rm -rf ~/.vim/pack/mybundle/start/vimpanel/.git
 
+git clone https://github.com/mihaifm/4colors ~/.vim/pack/mybundle/start/4colors
+rm -rf ~/.vim/pack/mybundle/start/4colors/.git
+
 git clone https://github.com/easymotion/vim-easymotion ~/.vim/pack/mybundle/start/vim-easymotion
 rm -rf ~/.vim/pack/mybundle/start/vim-easymotion/.git
+
+git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/mybundle/start/vim-airline
+rm -rf ~/.vim/pack/mybundle/start/vim-airline/.git
 
 # NetHack
 ln -sf $PWD/nethack/.nethackrc ~/.nethackrc
